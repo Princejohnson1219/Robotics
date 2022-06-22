3a3cf48ffec7e3afde913ad92ce96d00f7cf66aa
Princejohnson1219 <95515463+Princejohnson1219@users.noreply.github.com>
2022-06-22 10:03:02 +0530

    Add files via upload

---
+++ Robotics_Assignment/Assignment1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package robotics_prac1;
+
+import ch.aplu.robotsim.*;
+public class Assignment1 {
+    Assignment1(){
+        TurtleRobot t = new TurtleRobot();
+        while(true){
+        t.forward(200);
+        t.left(90);
+//        t.forward(200);
+//        t.left(90);
+//        t.forward(200);
+//        t.left(90);
+//        t.forward(200);
+//        t.left(90);
+//        t.forward(200);
+    }}
+public static void main(String args[]){
+Assignment1 m = new Assignment1();
+}
+}
+    
+
